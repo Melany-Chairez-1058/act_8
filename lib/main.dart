707376281b5c14f0f09757b43e783a351148fd48
MELANY_SARAHI_CHAIREZ_ARIAS <a.22308051281058@cbtis128.edu.mx>
2025-05-08 15:12:06 +0000

    8

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:act_8/ButtonAnimation.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonAnimation(Color.fromRGBO(57, 92, 249, 1),
+                  Color.fromRGBO(44, 78, 233, 1)),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonAnimation(Colors.yellow[700]!, Colors.yellow[800]!),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonAnimation(Colors.green[400]!, Colors.green[600]!),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonAnimation(Colors.red[700]!, Colors.red[800]!),
+            ],
+          ),
         ),
       ),
     );
